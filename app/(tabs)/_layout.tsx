c27e5fcaa7e7a1b9cdd7a4410ce962b8848a5564
Pluminkoo <mariematiwejko@gmail.com>
2026-06-13 15:24:53 +0200

    First draft Bottom Navigation with Bottom Sheet created, triggered by onPress of Services hub button

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,49 +1,67 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-
+import { BottomSheetHub } from '@/components/bottom-sheet';
 import { HapticTab } from '@/components/haptic-tab';
+import { SpecialTabButton } from '@/components/special-tab-button';
 import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import BottomSheet from '@gorhom/bottom-sheet';
+import { Tabs } from 'expo-router';
+import { useRef } from 'react';
+import { View } from 'react-native';
+
+
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+  const bottomSheetRef = useRef<BottomSheet>(null!);
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="prescriptions"
-        options={{
-          title: 'Prescriptions',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="pills.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="profile"
-        options={{
-          title: 'Profile',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="person.crop.circle.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+    <View style={{ flex: 1 }}>
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: Colors[colorScheme ?? 'dark'].tint,
+          headerShown: false,
+          tabBarButton: HapticTab,
+          tabBarStyle: {
+            height: 100,
+            borderTopColor: '#0a7ea4',
+            borderTopWidth: 2,
+            paddingTop: 20,
+          }
+        }}>
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: 'Home',
+            tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          }}
+        />
+        <Tabs.Screen
+          name="prescriptions"
+          options={{
+            href: null,
+            title: 'Prescriptions',
+            tabBarIcon: ({ color }) => <IconSymbol size={28} name="pills.fill" color={color} />,
+          }}
+        />
+        <Tabs.Screen
+          name='services-hub'
+          options={{
+            title: 'Services hub',
+            tabBarButton: () => (
+              <SpecialTabButton onPress={() => bottomSheetRef.current?.expand()} title='Services hub' />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: 'Profile',
+            tabBarIcon: ({ color }) => <IconSymbol size={28} name="person.crop.circle.fill" color={color} />,
+          }}
+        />
+      </Tabs>
+      <BottomSheetHub bottomSheetRef={bottomSheetRef} />
+    </View>);
+
 }
---
+++ app/(tabs)/services-hub.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+const Page = () => {
+    return (
+        <View>
+            <Text>Services hub</Text>
+        </View>
+    );
+};
+export default Page;
