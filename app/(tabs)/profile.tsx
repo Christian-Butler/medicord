403179c8f43c85c0dd26a8de5dbd8c2419a5b189
Christian Butler <N00211989@iadt.ie>
2026-07-08 15:37:45 +0200

    test

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -8,7 +8,7 @@ import { Link } from "expo-router";
 
 export default function ProfileScreen() {
   return (
-    <> </>
+    <><h1></h1> </>
     // <ParallaxScrollView
     //   headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
     //   headerImage={
