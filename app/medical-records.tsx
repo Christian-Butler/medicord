4c10ed9e03addd1604373fb60ed919a42bc19507
Pluminkoo <mariematiwejko@gmail.com>
2026-07-29 14:04:44 +0200

    Dummy pages transferred

---
+++ app/medical-records.tsx
@@ -0,0 +1,101 @@
+import React from "react";
+import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+import MaterialIcons from "@react-native-vector-icons/material-icons";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+interface MedicalList {
+    name: string;
+    icon: React.ComponentProps<typeof MaterialIcons>['name'];
+    button: React.ComponentProps<typeof MaterialIcons>['name'];
+    onPress?: () => void;
+}
+
+const records: MedicalList[] = [
+    {
+        name: 'Documents',
+        icon: 'article',
+        button: 'chevron-right',
+    },
+    {
+        name: 'Family / Personal history',
+        icon: 'person-search',
+        button: 'chevron-right',
+    },
+    {
+        name: 'Regular treatments',
+        icon: 'pending-actions',
+        button: 'chevron-right',
+    },
+    {
+        name: 'Allergies',
+        icon: 'coronavirus',
+        button: 'chevron-right',
+    },
+    {
+        name: 'Gynecological follow-up',
+        icon: 'female',
+        button: 'chevron-right',
+    },
+    {
+        name: 'Vaccines',
+        icon: 'vaccines',
+        button: 'chevron-right',
+    },
+    {
+        name: 'Surgical operations',
+        icon: 'monitor-heart',
+        button: 'chevron-right',
+    },
+    {
+        name: 'Lifestyle',
+        icon: 'directions-walk',
+        button: 'chevron-right',
+    },
+    {
+        name: 'Measurements',
+        icon: 'design-services',
+        button: 'chevron-right',
+    }
+];
+
+export default function MedicalRecordsScreen() {
+
+    return (
+        <SafeAreaProvider style={{ backgroundColor: '#EEF9FB' }}>
+            <ScrollView>
+                <View style={styles.container}>
+                    {records.map((record, index) => (
+                        <View key={`${record.name}-${index}`} style={styles.card}>
+                            <MaterialIcons name={record.icon} size={32} color="#5085A8" />
+                            <View>
+                                <Text>{record.name}</Text>
+                            </View>
+                            <TouchableOpacity>
+                                <MaterialIcons name={record.button} size={24} color="#3f3128" />
+                            </TouchableOpacity>
+                        </View>
+                    ))}
+                </View>
+            </ScrollView>
+        </SafeAreaProvider>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: '4%',
+        paddingLeft: '4%',
+        paddingRight: '4%',
+
+
+    },
+    card: {
+        flexDirection: 'row',
+        paddingTop: '4%',
+        marginBottom: '10%',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    }
+})
---
+++ app/medication-page.tsx
@@ -0,0 +1,36 @@
+import MaterialIcons from "@react-native-vector-icons/material-icons";
+import React from "react";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+
+export default function MedicationScreen() {
+    return (
+        <SafeAreaProvider style={{ backgroundColor: '#EEF9FB', paddingTop: '8%' }}>
+            <ScrollView>
+                <View style={styles.buttonContainer}>
+                    <MaterialIcons name="add" size={26} color="#0D5175" />
+                    <Text style={{ fontWeight: 500, color: '#0D5175', fontSize: 16 }}> Create Medication Routine </Text>
+                </View>
+            </ScrollView >
+        </SafeAreaProvider>
+
+    )
+}
+
+
+const styles = StyleSheet.create({
+    buttonContainer: {
+        alignSelf: 'center',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '90%',
+        borderRadius: 14,
+        borderWidth: 2,
+        paddingVertical: '4%',
+        paddingHorizontal: '4%',
+        borderColor: "#0D5175",
+        backgroundColor: '#fff',
+    },
+});
