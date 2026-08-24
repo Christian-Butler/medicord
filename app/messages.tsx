3211d33f5ecda087ea4bfd18a5becfcc54c851f9
Christian Butler <N00211989@iadt.ie>
2026-08-24 12:34:48 +0100

    added message functionality

--- app/messages.tsx
+++ app/messages.tsx
@@ -2,24 +2,20 @@ import ChatList from "@/components/chat-list";
 import ScreenHeader from "@/components/screen-header";
 import { Search } from "lucide-react-native";
 import React from "react";
-import { ScrollView, StyleSheet, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function Messages() {
   return (
     <SafeAreaProvider style={{ backgroundColor: "#EEF9FB" }}>
       <ScreenHeader title="Messages" />
-      <ScrollView>
-        <View>
-          <View
-            style={styles.search}
-            className="h-14 flex-row rounded-2xl border-2 items-center border-[#778888] bg-white px-5"
-          >
-            <Search size={18} color="#778888" />
-          </View>
-          <ChatList />
-        </View>
-      </ScrollView>
+      <View
+        style={styles.search}
+        className="h-14 flex-row rounded-2xl border-2 items-center border-[#778888] bg-white px-5"
+      >
+        <Search size={18} color="#778888" />
+      </View>
+      <ChatList />
     </SafeAreaProvider>
   );
 }
