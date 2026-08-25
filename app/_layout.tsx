9aaae369c18dfd7e24adf82d334a24caa8891ef3
Christian Butler <N00211989@iadt.ie>
2026-08-25 14:52:11 +0100

    added the new medication and your doctor pages to expo router.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -64,6 +64,8 @@ export default function RootLayout() {
             <Stack.Screen name="edit-appointment" options={{ headerShown: false }} />
             <Stack.Screen name="medication-routine" options={{ headerShown: false }} />
             <Stack.Screen name="medication-page" options={{ headerShown: false }} />
+            <Stack.Screen name="medication-formpage" options={{ headerShown: false }} />
+            <Stack.Screen name="medications" options={{ headerShown: false }} />
             <Stack.Screen name="medical-records" options={{ headerShown: false }} />
             <Stack.Screen name="messages" options={{ headerShown: false }} />
             <Stack.Screen name="chat-room" options={{ headerShown: false }} />
@@ -77,6 +79,7 @@ export default function RootLayout() {
             <Stack.Screen name="mr-lifestyle" options={{ headerShown: false }} />
             <Stack.Screen name="mr-measurements" options={{ headerShown: false }} />
             <Stack.Screen name="mr-search-history" options={{ headerShown: false }} />
+            <Stack.Screen name="your-doctors" options={{ headerShown: false }} />
           </Stack>
           <StatusBar style="auto" />
         </ThemeProvider>
